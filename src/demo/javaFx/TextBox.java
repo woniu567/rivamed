7ad81064aeb30909be66600766a592d5fac116ce
sunchaoyang <sunchaoyang@rivamed.cn>
2020-03-26 15:04:24 +0800

    javaFx文本框

--- src/demo/javaFx/TextBox.java
+++ src/demo/javaFx/TextBox.java
@@ -1,16 +1,40 @@
 package demo.javaFx;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class TextBox extends Application {
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(new Group(), 450, 250);
 
+        //TextField和PasswordField扩展了TextInput类，它是JavaFX中所有文本控件的超类
+        TextField notification = new TextField ();
+        notification.setText("Label");
+
+        notification.clear();
+
+        GridPane grid = new GridPane();
+        grid.setVgap(4);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(5, 5, 5, 5));
+        grid.add(new Label("To: "), 0, 0);
+        grid.add(notification, 1, 0);
+
+        Group root = (Group) scene.getRoot();
+        root.getChildren().add(grid);
+        stage.setScene(scene);
+        stage.show();
     }
 }
