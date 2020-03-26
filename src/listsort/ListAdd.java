691a63bfb5f061a6d25126bba89408449f2c6fc8
sunchaoyang <sunchaoyang@rivamed.cn>
2020-03-26 08:18:09 +0800

    list对实体去重

--- src/listsort/ListAdd.java
+++ src/listsort/ListAdd.java
@@ -1,12 +1,15 @@
 package listsort;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ListAdd {
 
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
+        Map<String,Person> map = new HashMap<>();
         personList.add(new Person("aa","aa","aa"));
         personList.add(new Person("bb","bb","bb"));
         personList.add(new Person("cc","cc","cc"));
@@ -16,12 +19,11 @@ public class ListAdd {
         personList.add(new Person("aa","aa","aa"));
         List<Person> persons = new ArrayList<>();
 
-        outLoop:for (Person p:personList){
-            if (!persons.contains(p)){
+        for (Person p:personList){
+            if (!map.containsKey(p.getName())){
                 persons.add(p);
-            }else {
-                continue outLoop;
             }
+            map.put(p.getName(),p);
         }
 
         for (Person person:persons){
