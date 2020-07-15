fa650468f7b65b40d9bee94d33b9891bec5d4e81
sunchaoyang <sunchaoyang@rivamed.cn>
2020-07-15 11:06:29 +0800

    list的精确去重

---
+++ src/listsort/ListDeduplication.java
@@ -0,0 +1,34 @@
+package listsort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//很好的去重
+public class ListDeduplication {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = stringList();
+        ArrayList<String> strList = new ArrayList<>();
+        strList.add("nmbxds");
+        strList.add("eikdds");
+        strList.add("jldsds");
+        for (String str:list){
+            if (!strList.contains(str)){
+                strList.add(str);
+            }
+        }
+        for (String s:strList){
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> stringList(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("yeiw");
+        list.add("jlds");
+        list.add("nmbx");
+        list.add("eikd");
+        list.add("cmdk");
+        return list;
+    }
+}
