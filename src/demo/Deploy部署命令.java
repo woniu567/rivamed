b8c370c2208ffe7d8a3d5eb0738245c4af98f1a0
sunchaoyang <sunchaoyang@rivamed.cn>
2020-12-10 16:59:36 +0800

    mysql添加索引的几种方式

--- src/demo/Deploy部署命令.java
+++ src/demo/Deploy部署命令.java
@@ -58,7 +58,21 @@ java -jar tm.war
 arthas执行成功的命令:
 trace cn.rivamed.tm.sysmanage.dictionaryManage.dao.DictionaryDao findAll '#cost>5' -n 5
 
+mysql添加索引
+1.添加PRIMARY KEY（主键索引）
+mysql>ALTER TABLE `table_name` ADD PRIMARY KEY ( `column` )
 
+2.添加UNIQUE(唯一索引)
+mysql>ALTER TABLE `table_name` ADD UNIQUE ( `column` )
+
+3.添加INDEX(普通索引)
+mysql>ALTER TABLE `table_name` ADD INDEX index_name ( `column` )
+
+4.添加FULLTEXT(全文索引)
+mysql>ALTER TABLE `table_name` ADD FULLTEXT ( `column`)
+
+5.添加多列索引
+mysql>ALTER TABLE `table_name` ADD INDEX index_name ( `column1`, `column2`, `column3` )
 
  */
 public class Deploy部署命令 {
